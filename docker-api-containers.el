397286872d462a5bc00419f629380ef77e87edbe
Philippe Vaucher <philippe@stvs.ch>
2016-04-21 08:51:40 +0200

    Implement most of containers api

--- docker-api-containers.el
+++ docker-api-containers.el
@@ -27,10 +27,52 @@
 
 ;; TODO add parameters: limit since before filters
 (defun docker-api-containers (&optional all size)
-  (docker-api-json-request 'get (format "/containers/json?all=%d&size=%d" (if all 1 0) (if size 1 0))))
+  (let ((endpoint (format "/containers/json?all=%d&size=%d" (if all 1 0) (if size 1 0))))
+    (docker-api-json-request 'get endpoint)))
+
+(defun docker-api-container-start (id &optional detach-keys)
+  (let ((endpoint (format "/containers/%s/start" id)))
+    (unless (null detach-key)
+      (setq endpoint (format "%s?detachKeys=%s" endpoint detach-keys)))
+    (docker-api-json-request 'post endpoint)))
+
+(defun docker-api-container-stop (id &optional timeout)
+  (let ((endpoint (format "/containers/%s/stop" id)))
+    (unless (null timeout)
+      (setq endpoint (format "%s?t=%d" timeout)))
+    (docker-api-json-request 'post endpoint)))
+
+(defun docker-api-container-restart (id &optional timeout)
+  (let ((endpoint (format "/containers/%s/restart" id)))
+    (unless (null timeout)
+      (setq endpoint (format "%s?t=%d" timeout)))
+    (docker-api-json-request 'post endpoint)))
+
+(defun docker-api-container-kill (id &optional signal)
+  (let ((endpoint (format "/containers/%s/kill" id)))
+    (unless (null timeout)
+      (setq endpoint (format "%s?signal=%s" signal)))
+    (docker-api-json-request 'post endpoint)))
+
+(defun docker-api-container-rename (id name)
+  (let ((endpoint (format "/containers/%s/rename?name=%s" id name)))
+    (docker-api-json-request 'post endpoint)))
+
+(defun docker-api-container-pause (id)
+  (let ((endpoint (format "/containers/%s/pause" id)))
+    (docker-api-json-request 'post endpoint)))
+
+(defun docker-api-container-unpause (id)
+  (let ((endpoint (format "/containers/%s/unpause" id)))
+    (docker-api-json-request 'post endpoint)))
+
+(defun docker-api-container-wait (id)
+  (let ((endpoint (format "/containers/%s/wait" id)))
+    (docker-api-json-request 'post endpoint)))
 
 (defun docker-api-container-remove (id &optional force remove-volumes)
-  (docker-api-json-request 'delete (format "/containers/%s?force=%d&v=%d" id (if force 1 0) (if remove-volumes 1 0))))
+  (let ((endpoint (format "/containers/%s?force=%d&v=%d" id (if force 1 0) (if remove-volumes 1 0))))
+    (docker-api-json-request 'delete endpoint)))
 
 (provide 'docker-api-containers)
 
